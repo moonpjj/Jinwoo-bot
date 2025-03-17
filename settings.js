ddbe4587c29c3f9b225d4a5e30b1fa66e9bdd928
moonpjj <go28go38@gmail.com>
2025-03-17 09:12:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'SILENT-SOBX-MD~aBhFFYJT#jdS2l649bHN81dA6ejz7PZgxg_r_lMdjuWrW9my0zFk' 
 //Enter your Jinwoo session id here; must start with MALVIN-XD~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -25,7 +25,7 @@ global.botname = process.env.BOT_NAME || 'ᴊɪɴᴡᴏᴏ-ʙᴏᴛ'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '263714757857' 
+global.ownernumber = process.env.OWNER_NUMBER || '201018696370' 
 
 //-----------------[ OWNER NAME ]------------------//
 
